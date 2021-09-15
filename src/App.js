dacfba37571668b7fd6f603be2e92703395ececd
antonio <jaia_@hotmail.com>
2021-09-14 19:41:09 -0600

    second commit

--- src/App.js
+++ src/App.js
@@ -18,9 +18,15 @@ function App() {
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
-          <Route path="/about"><h1>About</h1></Route>
-          <Route path="/users"><h1>users</h1></Route>
-          <Route path="/"><Home/></Route>
+          <Route path="/about">
+            <h1>About</h1>
+            </Route>
+          <Route path="/users">
+            <h1>users</h1>
+            </Route>
+          <Route path="/">
+            <Home/>
+          </Route>
         </Switch>
   
 
