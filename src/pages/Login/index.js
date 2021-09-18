1ce43a2c4e9a16a2cef9e1d3445726affd7a069f
Misa-El <mmancillaglz07@gmail.com>
2021-09-17 21:26:39 -0500

    login page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -7,7 +7,8 @@ import{
     Input,
     Label,
     Button,
-    Col
+    Col,
+    Row
 } from 'reactstrap'
 
 import Footer from '../../components/Footer'
@@ -17,21 +18,26 @@ import './styles.scss'
 function Login(){
     return(
         <>
-            <div className="container bg-light mt-3 p-3">
-            <Col xs="12">
+            <Row className="main-container d-flex">
+            <div className="container bg-light p-3">
+            <Col xs='12' >
                     <div>
                         <h1 className="font-weight-bold text-center">Welcome to DEV Community</h1>
                         <p className="text-center"><span className="text-relevant">DEV Comunity</span> is a community of 693,496 amazing developers</p>
                     </div>
+            <Col xs='12'>
                     <div className="group-btns d-flex flex-column">
                         <Button id="apple" className="mt-2" type="button" color="dark" size="lg" block>Continue whit Apple</Button>
                         <Button id="github" className="mt-2" type="button" color="secondary" size="lg" block>Continue whit GitHub</Button>
-                        <Button id="twitter" className="mt-2" type="button" color="primary" size="lg" block>Continue whit Twitter</Button>
+                        <Button id="twitter" className="mt-2" type="button"  size="lg" block>Continue whit Twitter</Button>
                     </div>
+            </Col>       
                     <div className="mt-3">
                     <p className="text-center">Have a password? Continue with your email address</p>
                     </div>
-                <Form className="p-3">
+            </Col>
+            <Col xs='12'>
+                <Form className="py-3">
                     <FormGroup className="mb-2">
                         <Label className="mb-2">Email</Label>
                         <Input className="text-here" name="email"/>
@@ -44,18 +50,18 @@ function Login(){
                         <input type="checkbox"/>
                         <label> Remember me</label>
                     </div>
-                    <Button className="mt-3" type="button" color="primary" size="lg" block >Continue</Button>
+                    <Button id= "continue"className="mt-3" type="button" size="lg" block >Continue</Button>
                     <p className="text-center mt-3"><span className="text-relevant">I forgot my password</span></p>
                 </Form>
             </Col>
                 <div>
-                    <div className="mb-5">
+                    <div className="mt-3 mb-5">
                         <p class="text-center text-muted ">Open Source <img src={sunglasses} alt="sunglasses"/> • Free Forever <img src={heart} alt="heart"/></p>
                         <p class="text-center text-muted ">We strive for transparency and don't collect excess data.</p>
                     </div>
                 </div>  
             </div>
-            
+            </Row>
             <Footer/>
         </>
     )
